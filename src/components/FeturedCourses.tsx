64d9a3d921a7114126c4e0359e1f8787f97d25db
Nagarjun A <nagarjun.avala.official@gmail.com>
2024-04-03 20:05:16 +0530

    Added Up Comming Webinars section of application

--- src/components/FeturedCourses.tsx
+++ src/components/FeturedCourses.tsx
@@ -18,11 +18,9 @@ const FeturedCourses = () => {
 
     return (
         <div className='py-12 bg-gray-900'>
-            <div>
-                <div className="text-center">
-                    <h2 className='text-base text-teal-600 font-semibold tracking-wide uppercase'>FEATURED COURSES</h2>
-                    <p className='mt-2 text-3xl leading-8 font-extrabold tracking-tight text-white sm:text-4xl'>Learn With the Best</p>
-                </div>
+            <div className="text-center">
+                <h2 className='text-base text-teal-600 font-semibold tracking-wide uppercase'>FEATURED COURSES</h2>
+                <p className='mt-2 text-3xl leading-8 font-extrabold tracking-tight text-white sm:text-4xl'>Learn With the Best</p>
             </div>
             <div className='mt-10'>
                 <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 justify-center">
