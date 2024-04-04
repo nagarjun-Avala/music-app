728f2dee39ec9d6e32f69867a3734e28f729ea07
Nagarjun A <nagarjun.avala.official@gmail.com>
2024-04-04 21:59:44 +0530

    Adding about us  page to the Application

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -2,8 +2,8 @@ import { BackgroundBeams } from "@/components/ui/background-beams";
 
 const ContactPage = () => {
     return (
-        <div>
-
+        <div className="min-h-screen bg-black py-12 pt-36">
+            <h1 className="text-lg md:text-7xl text-center font-sans font-bold mb-8 text-white">Contact Us</h1>
             <BackgroundBeams />
         </div>
     )
